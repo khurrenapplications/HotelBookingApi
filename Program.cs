4e6f6eba1216b93dd631728a22a2606803212c98
khurren1979 <khurren1979@gmail.com>
2026-07-18 00:21:40 +0100

    Refactor database configuration to use SQL Server and update connection string

--- Program.cs
+++ Program.cs
@@ -6,15 +6,10 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<HotelBookingDbContext>(options =>
-{
-    var connectionString = builder.Configuration.GetConnectionString("HotelBookingDb")
-        ?? "Server=localhost;Port=3306;Database=hotel_booking;User=root;Password=Strathmore21;";
-    var mysqlServerVersion = builder.Configuration["Database:MySqlServerVersion"] ?? "8.0.36";
 
-    // Pomelo needs the server version to generate SQL that matches the target MySQL instance.
-    options.UseMySql(connectionString, ServerVersion.Parse(mysqlServerVersion));
-});
+builder.Services.AddDbContext<HotelBookingDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("HotelBookingDb")));
 
 builder.Services.AddScoped<BookingService>();
 builder.Services.AddScoped<TestDataService>();
@@ -27,12 +22,12 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<HotelBookingDbContext>();
+//using (var scope = app.Services.CreateScope())
+//{
+//    var db = scope.ServiceProvider.GetRequiredService<HotelBookingDbContext>();
     // Keep the sample easy to run: MySQL tables are created on startup if the database exists.
-    await db.Database.EnsureCreatedAsync();
-}
+//    await db.Database.EnsureCreatedAsync();
+//}
 
 app.UseSwagger();
 app.UseSwaggerUI();
